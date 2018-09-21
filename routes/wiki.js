926a109c90810467795b7df983801b4e32ae0bc2
Joseph Park <josephpaulpark@gmail.com>
2018-09-21 14:06:27 -0500

    page slug

--- routes/wiki.js
+++ routes/wiki.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { db, Page, User } = require('../models');
 const addPage = require('../views/addPage');
 
-router.use(express.urlencoded({extended: false}))
+router.use(express.urlencoded({ extended: false }));
 
 router.get('/', async (req, res, next) => {
   console.log('ALL POSTS', await Page.findAll());
@@ -12,10 +12,24 @@ router.get('/', async (req, res, next) => {
 
 router.post('/', async (req, res, next) => {
   console.log('ADD A NEW POST');
- 
+
   console.log('FORM DATA', req.body);
-  
+
   // User.create()
+  const page = new Page({
+    title: req.body.title,
+    content: req.body.content,
+    slug: 'abc',
+  });
+
+  // make sure we only redirect *after* our save is complete!
+  // note: `.save` returns a promise.
+  try {
+    await page.save();
+    res.redirect('/');
+  } catch (error) {
+    next(error);
+  }
 
   // redirect if succes ? send a http 201
   //  redirect if error ? send ??
--- views/addPage.js
+++ views/addPage.js
@@ -1,15 +1,16 @@
-const html = require("html-template-tag");
-const layout = require("./layout");
+const html = require('html-template-tag');
+const layout = require('./layout');
 
-module.exports = () => layout(html`
+module.exports = () =>
+  layout(html`
   <h3>Add a Page</h3>
   <hr>
   <form method="POST" action="/wiki/">
-    
-    <div><input type="text" name="author"/></div>
-    
-    <div><input type="text" name="email"/></div>
-    
+
+    <div><input type="text" name="author"/>Author</div>
+
+    <div><input type="text" name="email"/>email</div>
+
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
       <div class="col-sm-10">
@@ -17,13 +18,16 @@ module.exports = () => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
-    
-    <div>PLACEHOLDER FOR PAGE STATUS INPUT FIELD</div>
+    <div><input type="text" name="content"></div>
+
+    <div><select name = "status">
+          <option value="open">open</option>
+          <option value="closed">closed</option>
+        </select></div>
 
     <div class="col-sm-offset-2 col-sm-10">
       <button type="submit" class="btn btn-primary">submit</button>
     </div>
-  
+
   </form>
 `);
