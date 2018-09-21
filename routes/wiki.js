6975a387155a16c9cc575f82ce602194ccfa0ae2
Paul Ebreo <paulebreo@triton.edu>
2018-09-21 11:23:35 -0500

    configure routes fro wiki

--- routes/wiki.js
+++ routes/wiki.js
@@ -6,6 +6,24 @@ router.get('/', async (req, res, next) => {
     console.log("ALL POSTS", await Page.findAll())
 
 })
+router.post('/', async (req, res, next) => {
+    console.log('ADD A NEW POST');
+    // serialize the body from 
 
+    // User.create()
+
+    // redirect if succes ? send a http 201
+    //  redirect if error ? send ??
+    next()
+    
+})
+
+router.get('/add', async (req, res, next) => {
+    console.log('SERVE ADD FORM');
+
+    // res.send(wikiAdd());
+    next()
+    
+})
 
 module.exports = router
