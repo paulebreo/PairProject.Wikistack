deebad27b41059cbfc5b10d7a823e8429e456e3d
Joseph Park <josephpaulpark@gmail.com>
2018-09-21 10:41:51 -0500

    add try catch and express

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const { db, Page, User } = require("./models");
+const express = require('express');
+const { db, Page, User } = require('./models');
+
+const app = express();
+const PORT = 3000;
 
 async function sync() {
+  try {
     db.authenticate().then(() => {
-        console.log("connected to the database");
+      console.log('connected to the database');
     });
     await Page.sync();
     await User.sync();
+
+    app.listen(PORT, () => {
+      console.log(`Server is lstening on port ${PORT}!`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 sync();
